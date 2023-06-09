dc1e018d07f3cb17a76360b68c6ed016b7975d27
RaviSupreme-15 <supremeravi1819@gmail.com>
2023-06-09 14:18:50 +0530

    first change done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
-  
+
+
+
+    public static void main(String args[]){
+
+
+        RWOnly rwOnly = new RWOnly();
+
+        rwOnly.setName("ravi");
+
+        System.out.println(rwOnly.getName());
+
+
+
+    }
+
+
+
+
 }
